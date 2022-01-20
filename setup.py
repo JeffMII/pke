335cc6144a02951dacdda3b02c6f589c2e2368e0
JeffMII <80857218+JeffMII@users.noreply.github.com>
2022-01-19 21:14:08 -0600

    Update setup.py
    Forgot the commas

--- setup.py
+++ setup.py
@@ -11,15 +11,15 @@ setup(name='pke',
                 'pke.unsupervised.statistical', 'pke.supervised.neural_based'],
       url="https://github.com/boudinfl/pke",
       install_requires=[
-            'six>=1.15.0, <=1.16.0'
-            'spacy>=3.0.0, <=3.2.1'
-            'nltk==3.6.3'
-            'networkx>=2.4.0, <=2.6.3'
-            'numpy==1.16.3'
-            'scipy>=1.4.1, <=1.5.4'
-            'scikit-learn>=0.22.1, <=1.0.2'
-            'unidecode>=1.1.1, <=1.3.2'
-            'future==0.18.2'
+            'six>=1.15.0, <=1.16.0',
+            'spacy>=3.0.0, <=3.2.1',
+            'nltk==3.6.3',
+            'networkx>=2.4.0, <=2.6.3',
+            'numpy==1.16.3',
+            'scipy>=1.4.1, <=1.5.4',
+            'scikit-learn>=0.22.1, <=1.0.2',
+            'unidecode>=1.1.1, <=1.3.2',
+            'future==0.18.2',
             'joblib>=0.14.1, <=1.1.0'
       ],
       package_data={'pke': ['models/*.pickle', 'models/*.gz']}
